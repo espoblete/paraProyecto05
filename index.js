84a1898ab98171ac9f53f0b63aed160b6b8d9a15
espoblete <es.poblete.a@gmail.com>
2023-01-08 12:23:08 -0300

    segundo commit y funcionando back y frontcha

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require ('express');
 const connectDb = require('./src/config/db.js');
+const cors = require("cors");
+const app = express();
 
 require ("dotenv").config();
-
-const routes = require("./src/resources/users/routes/index")
-
-const app = express();
+const routes = require("./src/resources/users/routes/rutes")
 
 connectDb();
 
+app.use (cors());
 app.use (express.json());
 app.use ("/", routes);
 
-
-
-
-app.listen (process.env.PORT, () =>{
+app.listen (process.env.PORT, () => {
 console.log (`escuchando en puerto ${process.env.PORT}`)
-})
+});
